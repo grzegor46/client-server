b239222c9cd69ea1b27910ed1007bc1759b0f4f1
Grzegor46 <grzegor46@gmail.com>
2023-06-27 22:57:33 +0200

    prepare variables for client and server

--- src/main/java/client/CLient.java
+++ src/main/java/client/CLient.java
@@ -1,4 +1,25 @@
 package client;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Properties;
+
 public class CLient {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socketCLient = null;
+        ServerSocket serverSocket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+
+        InputStream inputStream = new FileInputStream("src/main/resources/application-server.properties");
+        Properties prop = new Properties();
+        prop.load(inputStream);
+
+    }
+
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,4 +1,30 @@
 package server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Properties;
+
 public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socketCLient = null;
+        ServerSocket serverSocket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        InputStream inputStream = new FileInputStream("src/main/resources/application-server.properties");
+        Properties prop = new Properties();
+        prop.load(inputStream);
+
+        serverSocket = new ServerSocket(Integer.parseInt(prop.getProperty("server.port")));
+
+    }
+
+
+
+
+
 }
