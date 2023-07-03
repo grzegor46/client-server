47c4645f1246bb1d18b5c8ac811cf47b473f9225
Grzegor46 <grzegor46@gmail.com>
2023-07-03 19:49:27 +0200

    implemented simple response --> info and help

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -3,26 +3,31 @@ package server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDate;
 import java.util.Properties;
 import java.util.Scanner;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
+    private Socket socket = null;
+    private ServerSocket serverSocket = null;
+    private InputStreamReader inputStreamReader = null;
+    private OutputStreamWriter outputStreamWriter = null;
+    private BufferedReader bufferedReader = null;
+    private BufferedWriter bufferedWriter = null;
+    private String createdServerDate = LocalDate.now().toString();
+    private Scanner scanner = new Scanner(System.in);
+    private String applicationVersion;
 
-        Socket socket = null;
-        ServerSocket serverSocket = null;
-        InputStreamReader inputStreamReader = null;
-        OutputStreamWriter outputStreamWriter = null;
-        BufferedReader bufferedReader = null;
-        BufferedWriter bufferedWriter = null;
+    private void loadProperties() throws IOException {
         InputStream inputStream = new FileInputStream("src/main/resources/application-server.properties");
         Properties prop = new Properties();
         prop.load(inputStream);
-
         serverSocket = new ServerSocket(Integer.parseInt(prop.getProperty("server.port")));
+        applicationVersion = prop.getProperty("application.version");
+    }
 
-        Scanner scanner = new Scanner(System.in);
+    private void startServer() throws IOException {
 
         try {
 
@@ -34,14 +39,13 @@ public class Server {
             bufferedReader = new BufferedReader(inputStreamReader);
             bufferedWriter = new BufferedWriter(outputStreamWriter);
 
-
             while (true) {
 
                 String msgFromClient = bufferedReader.readLine();
 
                 System.out.println("Client: " + msgFromClient);
 
-                String msgToClient = scanner.nextLine();
+                String msgToClient = returnResponse(msgFromClient);
                 bufferedWriter.write(msgToClient);
                 bufferedWriter.newLine();
                 bufferedWriter.flush();
@@ -49,17 +53,50 @@ public class Server {
                 if (msgFromClient.equalsIgnoreCase("stop")) {
                     break;
                 }
-            }
-            socket.close();
-            inputStreamReader.close();
-            outputStreamWriter.close();
-            bufferedReader.close();
-            bufferedWriter.close();
 
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        closeConnection();
     }
 
+    private String returnResponse(String msgFromClient) {
+        switch (msgFromClient) {
+            case "help":
+                return getHelpCommand();
+            case "info":
+                return getInfoCommand();
+//            case "uptime":
+//                return getUpTimeCommand();
+//            case "stop":
+//                return getStopServerCommand();
+            default:
+                return scanner.nextLine();
+        }
+    }
+
+    private String getHelpCommand(){
+        return "_uptime_ - zwraca czas życia serwera, _info_ - zwraca numer wersji serwera, datę jego utworzenia, _help_ - zwraca listę dostępnych komend z krótkim opisem (tych komend, z tej listy, którą właśnie czytasz, czyli inaczej, komend realizowanych przez serwer), _stop_ - zatrzymuje jednocześnie serwer i klienta";
+    }
+
+    private String getInfoCommand(){
+
+        return "server was created: " + createdServerDate + ", version of this app is: " + applicationVersion ;
+    }
+
+    private void closeConnection() throws IOException {
+        socket.close();
+        inputStreamReader.close();
+        outputStreamWriter.close();
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.loadProperties();
+        server.startServer();
+    }
 
 }
