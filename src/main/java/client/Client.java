d9579b5e78993390c48569420b864a31cec42670
Grzegor46 <grzegor46@gmail.com>
2023-07-12 23:06:39 +0200

    refactor client side

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,45 +1,44 @@
 package client;
 
 
-import exception.NoConnectionToTheServer;
 import utils.LoadProperties;
-import java.io.*;
+import utils.Stream;
+
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
 
     private Socket socket = null;
-    private InputStreamReader inputStreamReader = null;
-    private OutputStreamWriter outputStreamWriter = null;
-    private BufferedReader bufferedReader = null;
-    private BufferedWriter bufferedWriter = null;
-    private int serverPort = LoadProperties.serverPort;
-    private String hostNameServer = LoadProperties.hostNameServer;
+    private final int serverPort = LoadProperties.serverPort;
+    private final String hostNameServer = LoadProperties.hostNameServer;
+    private Stream stream = null;
 
+    public static void main(String[] args) throws IOException {
+        Client client = new Client();
+        client.startConnection();
+    }
 
     private void startConnection() throws IOException {
 
         try {
 
             socket = new Socket(hostNameServer, serverPort);
-
-            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.stream = new Stream(socket);
 
             Scanner scanner = new Scanner(System.in);
             while (true) {
 
-                String msgToSend = scanner.nextLine();
-                bufferedWriter.write(msgToSend);
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
+                stream.bufferedWriter.write(scanner.nextLine());
+                stream.bufferedWriter.newLine();
+                stream.bufferedWriter.flush();
 
-
-                String msgFromServer = bufferedReader.readLine();
+                String msgFromServer = stream.bufferedReader.readLine();
                 if (msgFromServer == null) {
-//                    TODO not working after sent stop msg
-                    throw new NoConnectionToTheServer("No connection to the server");
+                    System.out.println("server is closed");
+                    scanner.close();
+                    break;
                 }
                 System.out.println("Server: " + msgFromServer);
             }
@@ -51,19 +50,12 @@ public class Client {
 
     private void closeConnection() {
         try {
-            bufferedReader.close();
-            bufferedWriter.close();
-            inputStreamReader.close();
-            outputStreamWriter.close();
+            stream.closeStreams();
             socket.close();
-        }catch(IOException e) {
-            System.out.println("attempt to close all stream failed");
+            System.out.println("connection closed");
+        } catch (IOException e) {
+            System.out.println("attempt to close all streams failed");
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        Client client = new Client();
-        client.startConnection();
     }
 }
