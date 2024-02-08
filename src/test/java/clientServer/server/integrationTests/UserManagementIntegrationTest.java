00b68b2d3ae585e8f05c8dac080644de4fd0e1c2
Grzegor46 <grzegor46@gmail.com>
2024-02-08 19:04:27 +0100

    integration test

--- src/test/java/clientServer/server/integrationTests/UserManagementIntegrationTest.java
+++ src/test/java/clientServer/server/integrationTests/UserManagementIntegrationTest.java
@@ -2,47 +2,66 @@ package clientServer.server.integrationTests;
 
 import client.Client;
 import org.junit.jupiter.api.*;
+import repository.Repository;
+import repository.UserRepository;
 import server.Server;
 import service.UserManagement;
 import utils.PropertiesUtils;
 import utils.Stream;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserManagementIntegrationTest {
 
+    private final Stream streamClient;
+    private final Repository userRepository = new UserRepository();
+
     @Test
-    void setUp() {
+    void setUp(){
         // before running these tests, please run first server manually or run gradle script "runServer"
     }
-
-    Socket socketCLient;
-    Stream streamClient;
 
     public UserManagementIntegrationTest() throws IOException {
 
         final int serverPort = PropertiesUtils.serverPort;
         final String hostNameServer = PropertiesUtils.hostNameServer;
 
-        socketCLient = new Socket(hostNameServer, serverPort);
+        Socket socketCLient = new Socket(hostNameServer, serverPort);
         streamClient = new Stream(socketCLient);
 
     }
 
     @Test
-    void test() throws IOException {
+    void shouldCreateUserWithSuccess() throws IOException {
 
-        String msgToServer = "help";
+        String msgToServer = "create user";
         streamClient.printWriter.println(msgToServer);
 
         String msgFromServer = streamClient.bufferedReader.readLine();
-        System.out.println(msgFromServer);
-
+        streamClient.printWriter.println("user_nickname");
+        msgFromServer = streamClient.bufferedReader.readLine();
+        streamClient.printWriter.println("user_password");
+        msgFromServer = streamClient.bufferedReader.readLine();
+        assertEquals(msgFromServer,"User created");
+        List<String> lines = Files.readAllLines(Path.of(PropertiesUtils.databasePath));
+        for (String line : lines) {
+            if (line.contains("user_nickname")) {
+                System.out.println("yes!");
+            }
+        }
+        new FileWriter(PropertiesUtils.databasePath, false).close();
     }
 
     @Test
@@ -54,7 +73,7 @@ public class UserManagementIntegrationTest {
         String msgFromServer = streamClient.bufferedReader.readLine();
         System.out.println(msgFromServer);
 
-        Assertions.assertEquals(msgFromServer, "{\"createdServerDate\":\"2024-02-06\",\"appVersion\":\"1.1.0\"}");
+        assertEquals(msgFromServer, "{\"createdServerDate\":\"2024-02-06\",\"appVersion\":\"1.1.0\"}");
 
 
     }
@@ -63,5 +82,4 @@ public class UserManagementIntegrationTest {
     void stopTheServer() {
         streamClient.printWriter.println("stop");
     }
-
 }
